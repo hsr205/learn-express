2db9c5d8dc03c413568a2eedde28f6ca7d5d16e8
Henry Rothenberg <hsr205@aol.com>
2025-02-19 10:22:31 -0500

    Add logic

--- quiz/types.ts
+++ quiz/types.ts
@@ -1,3 +1,16 @@
-import { Request } from 'express';
+import {Request} from 'express';
 
-// define relevant types here
+export default interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+}
+
+/**
+ * A type that represents the request received by the server
+ */
+export default interface UserRequest extends Request {
+    users?: User[];
+}
